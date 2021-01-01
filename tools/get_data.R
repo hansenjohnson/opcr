3ffac6ff846f36deba8601da81999e73360060c0
Hansen Johnson <hansen.johnson@dal.ca>
2021-01-01 18:25:59 -0500

    use new example data

--- tools/get_data.R
+++ tools/get_data.R
@@ -1,5 +1,5 @@
 ## get_data ##
 # get and process example data used in opcr
 library(opcr)
-opc = opc_process('../gosl_habitat/data/raw/2019b/opc/OPC027.D00')
+opc = opc_process('../gosl_habitat/data/raw/2018/opc/OPC033.D00')
 save(opc,file='data/opc.rda')
